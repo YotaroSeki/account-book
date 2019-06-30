9541c29292133bdd4c2451ae7c4c3f715bd27850
YotaroSeki <youyoutarou.dog0115@gmail.com>
2019-06-30 18:36:38 +0900

    Fix bug

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -71,6 +71,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:price, :name, :user_id, :purchased_date)
+      params.require(:item).permit(:price, :name, :purchased_date)
     end
 end
